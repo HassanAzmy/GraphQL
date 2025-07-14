3dafe1eb75bb2cf3c6a281397bbb38657586fa12
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-14 06:59:56 +0300

    Defining a mutation schema

---
+++ Backend/graphql/schema.js
@@ -0,0 +1,36 @@
+import { buildSchema } from "graphql";
+
+export default buildSchema(`
+   type Post {
+      _id: ID
+      title: String!
+      content: String!
+      imageURL: String!
+      creator: User!
+      createAt: String!
+      updatedAt: String!
+   }
+   
+   type User {
+      _id: ID!
+      name: String!
+      email: String!
+      password: String
+      status: String!
+      posts: [Post!]!
+   }
+
+   input UserInputData {
+      email: String!
+      name: String!
+      password: String!
+   } 
+   
+   type RootMutation {
+      createUser(userInput: UserInputData): User
+   }
+
+   schema {
+      mutation: RootMutation
+   }   
+`);
